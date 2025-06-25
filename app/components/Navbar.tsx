3f477fa4951ee97212c63e72343617c7787e041f
OussamaMehiri <oussamamehiri@gmail.com>
2025-06-25 13:59:35 +0100

    add social media links with README file

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -14,22 +14,30 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="fixed right-0 left-0 bottom-0   flex justify-center  items-center p-2 border border-black backdrop-blur-xl my-2 mx-1 rounded-sm lg:bottom-auto lg:justify-between z-20 overflow-hidden  " role="navigation">
-      <div className="bg-white top-0 right-0 left-0 bottom-0 absolute -z-10 opacity-50  "/>
+    <nav
+      className="fixed right-0 left-0 bottom-0   flex justify-center  items-center p-2 border border-black backdrop-blur-xl my-2 mx-1 rounded-sm lg:bottom-auto lg:justify-between z-20 overflow-hidden  "
+      role="navigation"
+    >
+      <div className="bg-white top-0 right-0 left-0 bottom-0 absolute -z-10 opacity-50  " />
       {/* navbar title */}
       <div className=" hidden  lg:flex">
-      <h1 className="font-semibold">Oussama</h1>
-        <Gavel/>
-      <h1 className="font-semibold">Mehiri </h1>
+        <h1 className="font-semibold">Oussama</h1>
+        <Gavel />
+        <h1 className="font-semibold">Mehiri </h1>
       </div>
       {/* navbar links */}
       <div>
-        <ul className="flex flex-row gap-5
-         ">
+        <ul
+          className="flex flex-row gap-5
+         "
+        >
           <li>
-            <Link href={"#home"} className="flex flex-col items-center justify-center ">
-              <House className="hover:text-neutral-400 transition duration-150 "/>
-              <p className="hidden  sm:flex" >Home</p>
+            <Link
+              href={"#home"}
+              className="flex flex-col items-center justify-center "
+            >
+              <House className="hover:text-neutral-400 transition duration-150 " />
+              <p className="hidden  sm:flex">Home</p>
             </Link>
           </li>
           {/* <li>
@@ -39,9 +47,12 @@ const Navbar = () => {
             </Link>
           </li> */}
           <li>
-            <Link href={"#skills"} className="flex flex-col items-center justify-center ">
-              <Bubbles className=""/>
-              <p className="hidden  sm:flex" >Skills</p>
+            <Link
+              href={"#skills"}
+              className="flex flex-col items-center justify-center "
+            >
+              <Bubbles className="" />
+              <p className="hidden  sm:flex">Skills</p>
             </Link>
           </li>
           {/* <li>
@@ -51,9 +62,12 @@ const Navbar = () => {
             </Link>
           </li> */}
           <li>
-            <Link href={"#resume"} className="flex flex-col items-center justify-center ">
-              <NotepadText className=""/>
-              <p className="hidden  sm:flex" >Resume</p>
+            <Link
+              href={"#resume"}
+              className="flex flex-col items-center justify-center "
+            >
+              <NotepadText className="" />
+              <p className="hidden  sm:flex">Resume</p>
             </Link>
           </li>
           {/* <li>
@@ -70,8 +84,8 @@ const Navbar = () => {
           <Moon />
         </Button> */}
         <Link href={"#contact"}>
-                <Button className="cursor-pointer">Work With ME</Button>
-</Link>
+          <Button className="cursor-pointer">Work With ME</Button>
+        </Link>
       </div>
     </nav>
   );
--- app/pages/Contact.tsx
+++ app/pages/Contact.tsx
@@ -1,32 +1,61 @@
 // import Link from "next/link";
-import {  Waypoints } from "lucide-react";
-import Link from "next/link";
+import { Waypoints } from "lucide-react";
+import { MdEmail } from "react-icons/md";
 import React from "react";
 
-import { FaGithub, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Contact = () => {
   return (
     <div className=" ">
       <div className="flex flex-row justify-center items-center gap-2 px-5">
         <div className="border-t-1 border-black border-solid flex-grow" />
-              <Waypoints className=""/>
+        <Waypoints className="" />
         <h2 className="text-4xl">Contact</h2>
         <hr className="border-t-1 border-black border-solid flex-grow" />
       </div>
       <div className="flex flex-wrap gap-8 justify-center p-5">
-        <Link href={""}>
+        <a
+          href="https://github.com/OussamaMehiri"
+          target="_blank"
+          rel="noopener noreferer"
+          aria-label="Visit my Github profile"
+        >
+          {" "}
           <FaGithub size={50} />
-        </Link>
-        <Link href={""}>
-          <FaLinkedin  size={50} />
-        </Link>
-        <Link href={""}>
-          <FaTwitter  size={50} />
-        </Link>
-        <Link href={""}>
-          <FaInstagram size={50}  />
-        </Link>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/oussamamehiri/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Visit my LinkedIn profile"
+        >
+          <FaLinkedin size={50} />
+        </a>{" "}
+        {/* <a
+          href="https://www.linkedin.com/in/oussamamehiri/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Visit my X profile"
+        >
+          <FaTwitter size={50} /> */}
+        {/* </a>{" "} */}
+        {/* <a
+          href="https://www.linkedin.com/in/oussamamehiri/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Visit my IG profile"
+        >
+          <FaInstagram size={50} />
+        </a> */}
+        <a
+          href="mailto:oussamamehiri@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Send me an email"
+        >
+          <MdEmail size={50} />
+        </a>
       </div>
     </div>
   );
